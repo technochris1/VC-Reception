1100c0e6b23e45b3b9cad4f290b804568002ec78
technochris1 <technochris1@gmail.com>
2024-08-26 16:59:01 -0400

    getting somewhere

--- server-app.py
+++ server-app.py
@@ -1,7 +1,8 @@
 import os
 import uuid
+import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func
@@ -29,30 +30,87 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/guest/')
-@app.route('/guest/<guid>')
-def hello(guid=None):
-    return render_template('guest-view.html', guid=guid)
+
+
+@app.route('/guests/')
+def guests():
+    return render_template('guests.html')
+
+@app.route('/forms/')
+def forms():
+    return render_template('forms.html')
+
+
+
+
+
+@app.route('/guest/', methods=['GET', 'POST'])
+def guest():
+    if request.method == 'POST':
+        username = request.values.get('user') # Your form's
+        password = request.values.get('pass') # input names
+        #your_register_routine(username, password)
+
+        print(request.values.get('emailAddress'))
+
+        existingGuest = db.session.execute(db.select(VcGuest).filter_by(email=request.values.get('emailAddress'))).first()
+        print(existingGuest)
+        if(existingGuest == None):
+            newGuest = VcGuest(firstName=request.values.get('firstName'),
+            lastName=request.values.get('lastName'),
+            email=request.values.get('emailAddress'),
+            phone=request.values.get('phoneNumber'),
+            termsCheck=bool(request.values.get('termsCheck')),
+            idCheck=bool(request.values.get('idCheck'))
+            )
+
+
+            db.session.add(newGuest)
+            db.session.commit()
+
+
+        
+
+
+        return render_template('guest-view.html')
+    else:
+        # You probably don't have args at this route with GET
+        # method, but if you do, you can access them like so:
+        #yourarg = flask.request.args.get('argname')
+        #your_register_template_rendering(yourarg)
+        
+        return render_template('guest-view.html')
+
+@app.route('/checkIn/<uuid>')
+def checkIn(uuid = None):
+    guest = VcGuest.query.filter_by(uuid = uuid).first()
+    if(guest):
+        return json.dumps(guest.firstName)
+    else:
+        return abort(404)
 
 
 
 def generate_uuid():
     return str(uuid.uuid4())
 
+
+
 class VcGuest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String, name="uuid", default=generate_uuid)
 
-    firstname = db.Column(db.String(100))
-    lastname = db.Column(db.String(100))
-    email = db.Column(db.String(200))
+    firstName = db.Column(db.String(100))
+    lastName = db.Column(db.String(100))
+    email = db.Column(db.String(200), unique=True, nullable=False)
     phone = db.Column(db.String(20))
-    age = db.Column(db.Integer)
+    termsCheck = db.Column(db.Boolean(), default=False)
+    idCheck = db.Column(db.Boolean(), default=False)
+
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    bio = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<Vc Guest - {self.firstname}>'
+        return f'<Vc Guest - {self.firstName}>'
 
 
 with app.app_context():
